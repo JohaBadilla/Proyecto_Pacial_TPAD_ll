321ef2e5cb78116fb79116fb442f5758d904f8a9
JohaBadilla <Johanna.ubadilla@gmail.com>
2024-07-02 23:48:45 -0600

    programa en python para convertit grados

---
+++ convertir.py
@@ -0,0 +1,8 @@
+# Solicitar la temperatura en grados Celsius al usuario
+celsius = float(input("Ingrese la temperatura en grados Celsius:"))
+
+# Convertir la temperatura a grados Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+# Mostrar el resultado
+print(celsius,"Los  ",celcius," grados Celsius son",fahrenheit,"grados Fahrenheit")
